f3a632973e3f23ce2f2f8543de9c9efbf59739d1
breakxing <breakxing1102@gmail.com>
2023-12-01 20:35:48 +0800

    Add correctness 31 ~ 34

--- src/correctness/exp1.py
+++ src/correctness/exp1.py
@@ -4,24 +4,27 @@ from test_util import *
 import os
 
 
-file_seg = 5
+file_seg = 3
 chunk_seg = 12
 
 ini_path = "res.ini"
-state_paths = [f"/mnt/state{i % 4}/path{i}" for i in range(1 << file_seg)]
+# state_paths = [f"/mnt/state{i % 4}/path{i}" for i in range(1 << file_seg)]
+state_paths = [f"./state/path{i}" for i in range(1 << file_seg)]
 state_paths = ",".join(state_paths)
 
-cir_path = "./MPI_circuit/ori/27/qft27.txt"
-
+# cir_path = "./MPI_circuit/ori/27/qft27.txt"
+cir_path = "cir.txt"
 cir = get_circuit()
-N = 27
-mpiqubit = 1
+N = 17
+mpiqubit = 2
+for _ in range(10):
+    H(cir,N - 1)
 # for _ in range(100):
 #     H(cir,12)
 # for i in range(N):
 #     H(cir,i)
 # CPhase(cir,5,N - 1,math.pi)
-# create_circuit(cir, cir_path)
+create_circuit(cir, cir_path)
 # exit(0)
 os.chdir("..")
 # os.system("make clean")
@@ -30,7 +33,7 @@ os.chdir("correctness")
 
 for n in [N]:
     args = Args(total_qbit=n, file_qbit=file_seg, chunk_qbit=chunk_seg,mpi_qbit=mpiqubit,
-                is_subcircuit=0,MPI_testing=0,
+                is_subcircuit=0,MPI_testing=1,
                 runner_type="MPI" if mpiqubit != 0 else "IO", state_paths=state_paths)
     ini = Ini(args, ini_path)
     ini.out()
@@ -39,23 +42,23 @@ for n in [N]:
         # simple_test(f"[H {n} subcircuit]", False, cir_path, state_paths, n, (1 << file_seg))
     else:
         os.chdir("..")
-        os.system("scp -r -P 9048 ./*.cpp rdma2:~/SubQuokka_dev/src/")
-        os.system("scp -r -P 9048 ./*.hpp rdma2:~/SubQuokka_dev/src/")
-        os.system("scp -r -P 9048 ./*.h rdma2:~/SubQuokka_dev/src/")
-        os.system("scp -r -P 9048 ./makefile rdma2:~/SubQuokka_dev/src/")
-        os.system(f"scp -r -P 9048 ./correctness/{ini_path} rdma2:~/SubQuokka_dev/src/correctness/")
+        # os.system("scp -r -P 9048 ./*.cpp rdma2:~/SubQuokka_dev/src/")
+        # os.system("scp -r -P 9048 ./*.hpp rdma2:~/SubQuokka_dev/src/")
+        # os.system("scp -r -P 9048 ./*.h rdma2:~/SubQuokka_dev/src/")
+        # os.system("scp -r -P 9048 ./makefile rdma2:~/SubQuokka_dev/src/")
+        # os.system(f"scp -r -P 9048 ./correctness/{ini_path} rdma2:~/SubQuokka_dev/src/correctness/")
         # os.system(f"scp -r -P 9048 ./correctness/{cir_path} rdma2:~/SubQuokka_dev/src/correctness/")
-        input("Go other computer to make")
+        # input("Go other computer to make")
         os.chdir("correctness")
-        os.system(f"mpirun -x LD_LIBRARY_PATH --bind-to none --hostfile ../hf --map-by ppr:1:node \"$(pwd)/../Quokka\" -i {ini_path} -c {cir_path}")
-        # os.system(f"mpirun -np {1 << mpiqubit} --bind-to none ../Quokka -i {ini_path} -c ../../subcircuitFinder/abc.txt")
-        # for i in range(8,16):
-        #     os.system(f"cp ./state1/path{i - 8} ./state0/path{i}")
-        #     os.system(f"cp ./state2/path{i - 8} ./state0/path{i + 8}")
-        #     os.system(f"cp ./state3/path{i - 8} ./state0/path{i + 16}")
-        # state_path_compare = [f"./state0/path{i}" for i in range((1 << file_seg) * (1 << args.mpi_qbit))]
-        # state_path_compare = ",".join(state_path_compare)
-        # simple_test(f"[H {n} subcircuit]", False, cir_path, state_path_compare, n, (1 << file_seg) * (1 << args.mpi_qbit))
+        # os.system(f"mpirun -x LD_LIBRARY_PATH --bind-to none --hostfile ../hf --map-by ppr:1:node \"$(pwd)/../Quokka\" -i {ini_path} -c {cir_path}")
+        os.system(f"mpirun -np {1 << mpiqubit} --bind-to none ../Quokka -i {ini_path} -c {cir_path}")
+        for i in range(8,16):
+            os.system(f"cp ./state1/path{i - 8} ./state0/path{i}")
+            os.system(f"cp ./state2/path{i - 8} ./state0/path{i + 8}")
+            os.system(f"cp ./state3/path{i - 8} ./state0/path{i + 16}")
+        state_path_compare = [f"./state0/path{i}" for i in range((1 << file_seg) * (1 << args.mpi_qbit))]
+        state_path_compare = ",".join(state_path_compare)
+        simple_test(f"[H {n} subcircuit]", False, cir_path, state_path_compare, n, (1 << file_seg) * (1 << args.mpi_qbit))
 
 
 # -subcircuit
